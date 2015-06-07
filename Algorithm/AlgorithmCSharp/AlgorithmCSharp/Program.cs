44479844c0b14dad095578ad2f5bad758842423d
murata83 <83.program@gmail.com>
2015-06-08 01:15:03 +0900

    アナグラム発見 C#

--- Algorithm/AlgorithmCSharp/AlgorithmCSharp/Program.cs
+++ Algorithm/AlgorithmCSharp/AlgorithmCSharp/Program.cs
@@ -7,18 +7,18 @@ namespace AlgorithmCSharp
         static void Main(string[] args)
         {
             // 魔法陣
-            new Q1.Zentansaku().Start();
-            new Q1.BackTrack().Start();
+            //new Q1.Zentansaku().Start();
+            //new Q1.BackTrack().Start();
 
             // n クイーン問題
             //new Q2.Zentansaku().Start(4);
             //new Q2.BackTrack().Start(4);
 
-            // 数当てゲーム
-            //new Q3().Start(1000000);
-
             // トロミノパズル
-            //new Q4().Start(2);
+            //new Q3().Start(2);
+
+            // アナグラム発見
+            new Q4().Start("Q4.txt");
 
 
             Console.ReadLine();
---
+++ Algorithm/AlgorithmCSharp/AlgorithmCSharp/Q4.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace AlgorithmCSharp
+{
+    /// <summary>
+    /// アナグラム発見
+    /// </summary>
+    class Q4
+    {
+        public void Start(string filePath)
+        {
+            var wordList = File.ReadLines(filePath);
+            var anagramGroupList = this.ComputeAnagramGroupList(wordList);
+
+            foreach (var group in anagramGroupList)
+            {
+                Console.WriteLine(group.Key + "：" + "{" + string.Join(", ", group) + "}");
+            }
+        }
+
+        private IGrouping<string, string>[] ComputeAnagramGroupList(IEnumerable<string> wordList)
+        {
+            return wordList
+                .GroupBy(
+                    word => new string(word.OrderBy(c => c).ToArray()),
+                    word => word
+                )
+                .ToArray();
+        }
+    }
+}
